d8e0246084d6aa8e9fcab71353394e23f5677e50
tangxiaochao <Tang-XC@outlook.com>
2024-05-21 11:47:45 +0800

    feat:初始化项目,添加logo

---
+++ src/pages/hot/hot.vue
@@ -0,0 +1,32 @@
+<template>
+  <view>
+    <view class="hot-title">
+      <view class="hot-icon">
+        <image class="img" src="@/static/images/tab-icons/hot-active.png" />
+      </view>
+      <text>iMooc Blog</text>
+    </view>
+  </view>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello world',
+    };
+  },
+};
+</script>
+<style scoped lang="scss">
+.hot-title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 0;
+}
+.hot-icon {
+  width: 30px;
+  height: 30px;
+  margin-right: 8px;
+}
+</style>
